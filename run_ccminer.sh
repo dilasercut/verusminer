e0a4a16cb46cd435e678376802bf3ca384bacaec
dilasercut <dilasercutting39@gmail.com>
2024-12-22 23:20:24 +0700

    Update run_ccminer.sh

--- run_ccminer.sh
+++ run_ccminer.sh
@@ -1,1 +1,11 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RX32sG2pp33mmQ1grNm4Uo4UKZ7h4voiNy.worker_name -p x -t 4
+#!/bin/bash
+
+# Ganti wallet dan worker_name di sini
+WALLET="RX32sG2pp33mmQ1grNm4Uo4UKZ7h4voiNy"
+WORKER="stb_b860h_01"
+POOL="stratum+tcp://ap.luckpool.net:3956"
+
+# Jalankan miner
+cd /root/miner/ccminer
+./ccminer -a verus -o \$POOL -u \$WALLET.\$WORKER -p x -t 3
+EOF
